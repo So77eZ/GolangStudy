d74ba178bdfe7c7b9834dd7befd08443f6be8345
Slava <ja227@mail.ru>
2026-06-24 20:56:49 +0300

    upgraded main script with user input

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 
 func main() {
 	const IMTpowder  = 2
-	userHeight := 1.8
-	userWeight := 100.0
+	var userHeight float64 
+	var userWeight float64
+
+	//Ввод пользователя
+	fmt.Print("Калькулятор индекса массы тела\n")
+	fmt.Print("Введите свой рост в метрах (Пример - 1.71): ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в кг (Пример - 62): ")
+	fmt.Scan(&userWeight)
+
 	IMT := userWeight / math.Pow(userHeight,IMTpowder)
-	fmt.Print(IMT)
+	fmt.Print("Ваш индекс массы тела: ")
+	fmt.Print(math.Floor(IMT))
 }
