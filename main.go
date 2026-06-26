b757267407cbe9f024639fd5e56b16293d197b9d
Slava <ja227@mail.ru>
2026-06-26 23:17:54 +0300

    added swith/case user diagnosis output

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ func main() {
 	fmt.Println("Калькулятор индекса массы тела")
 	userWeight,userHeight := getUserInput()
 	IMT := calculateIMT(userWeight,userHeight)
-	outputResult(IMT)
+	outputResult(IMT,printUserDiagnosis(IMT))
 }
 
-func outputResult(imt float64){
+func outputResult(imt float64, diagnosis string){
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f",imt)
+	fmt.Println(diagnosis)
 	fmt.Print(result)
 }
 
@@ -32,4 +33,37 @@ func getUserInput() (float64,float64){
 	fmt.Print("Введите свой вес в кг (Пример - 62): ")
 	fmt.Scan(&userWeight)
 	return userWeight,userHeight
+}
+
+func printUserDiagnosis(IMT float64)string{
+	switch {
+	case IMT < 16:                  return "У вас сильный дефицит массы тела"
+	case IMT >= 16   && IMT < 18.5: return "У вас дефицит массы тела"
+	case IMT >= 18.5 && IMT < 25:	return "У вас нормальная масса тела"
+	case IMT >= 25   && IMT < 30: 	return "У вас избыточная масса тела"
+	case IMT >= 30   && IMT < 35: 	return "У вас 1-я степень ожирения"
+	case IMT >= 35   && IMT < 40: 	return "У вас 2-я степень ожирения"
+	default:                      	return "У вас 3-я степень ожирения"
+	}
+	// if IMT < 16{
+	// 	return "У вас сильный дефицит массы тела"
+	// }
+	// if IMT >= 16 && IMT < 18.5{
+	// 	return "У вас дефицит массы тела"
+	// }
+	// if IMT >= 18.5 && IMT < 25{
+	// 	return "У вас нормальная масса тела"
+	// }
+	// if IMT >= 25 && IMT < 30{
+	// 	return "У вас избыточная масса тела"
+	// }
+	// if IMT >= 30 && IMT < 35{
+	// 	return "У вас 1-я степень ожирения"
+	// }
+	// if IMT >= 35 && IMT < 40{
+	// 	return "У вас 2-я степень ожирения"
+	// }
+	// //if IMT > 40{
+	// return "У вас 3-я степень ожирения"
+	// //}
 }
