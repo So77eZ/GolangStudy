65d559f127b6045f969eb4c461a60b4cd109775f
Slava <ja227@mail.ru>
2026-07-05 00:49:40 +0300

    feat: add transaction tracker and remove obsolete IMT calculator
    replace root IMT calculator with a transaction tracker in massives/main.go
    
    remove obsolete main.go and IMTcalculator/go.mod

--- massives/main.go
+++ massives/main.go
@@ -7,10 +7,11 @@ import (
 
 func main() {
 	var transactions []float64
+	fmt.Print("Программа учета транзакций")
 	for {
 		fmt.Print("Введите транзакцию (или 'q' для выхода): ")
 		input := getUserInput()
-		if input == "q" {
+		if input == "q" || input == "Q" || input == "" {
 			break
 		} else {
 			input, err := strconv.ParseFloat(input, 64)
@@ -24,6 +25,7 @@ func main() {
 	if len(transactions) > 0 {
 		fmt.Println("Массив введенных транзакций:")
 		fmt.Println(transactions)
+		fmt.Println("Баланс кошелька:", getUserWalletBlance(transactions))
 	}
 }
 
@@ -31,4 +33,12 @@ func getUserInput() string {
 	var userInputNumber string
 	fmt.Scan(&userInputNumber)
 	return userInputNumber
+}
+
+func getUserWalletBlance(transactions []float64) float64 {
+	balance := 0.0
+	for _, transaction := range transactions {
+		balance += transaction
+	}
+	return balance
 }
