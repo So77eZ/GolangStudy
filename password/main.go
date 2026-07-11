eea58ca1d3cef49d082c30082fcb34bb5a2cb4ab
Slava <ja227@mail.ru>
2026-07-11 21:53:26 +0300

    small fixes using pointers

--- password/main.go
+++ password/main.go
@@ -6,38 +6,57 @@ import (
 )
 
 //объявление структуры для хранения данных аккаунта
-type accoutData struct {
+type accountData struct {
 	login    string
 	password string
 	url      string
 }
 
-var symbols = []rune("1abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+")
+func (account *accountData) inputAccountData() {
+	account.login = getUserInput("Input login: ")
+	account.generatePassword()
+	account.url = getUserInput("Input url: ")
+}
+
+func (account accountData) printAccountData() {
+	fmt.Println("Login:", account.login)
+	fmt.Println("Password:", account.password)
+	fmt.Println("URL:", account.url)
+}
+
+// ф-ция генерации пароля для поля "password" в стуктуре accountData.
+func (account *accountData) generatePassword() {
+	var symbols = []rune("1abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+")
+	generatedPassword := make([]rune, 12) //Длинна пароля
+	for i := range generatedPassword {
+		generatedPassword[i] = symbols[rand.IntN(len(symbols))]
+	}
+	account.password = string(generatedPassword)
+}
 
 func main() {
 
-	rand.IntN(100)
-
-	UserAccountData := accoutData{}
+	userAccountData := accountData{}
 
 	fmt.Println("Вход в личный кабинет")
 	for {
-		fmt.Println("\nВыберите действие:")
-		fmt.Println("1. Добавить данные аккаунта")
-		fmt.Println("2. Просмотреть данные аккаунта")
-		fmt.Println("3. Выход")
+		printMenu()
 		var choice int
 		fmt.Scan(&choice)
+		if choice < 1 || choice > 3 {
+			fmt.Println("\nОшибка ввода: выберите пункт меню от 1 до 3")
+			continue
+		}
 		if choice == 1 {
-			getAccountData(&UserAccountData)
+			userAccountData.inputAccountData()
 			fmt.Println("\nДанные аккаунта добавлены успешно")
 		}
 		if choice == 2 {
 			fmt.Println("\nПросмотр данных аккаунта")
-			printAccountData(&UserAccountData)
+			userAccountData.printAccountData()
 		}
 		if choice == 3 {
-			fmt.Println("Выход из программы")
+			fmt.Println("\nВыход из программы")
 			break
 		}
 	}
@@ -50,27 +69,9 @@ func getUserInput(prompt string) string {
 	return result
 }
 
-func getAccountData(account *accoutData) {
-	login := getUserInput("Input login: ")
-	password := generatePassword(12) //getUserInput("Input password: ")
-	url := getUserInput("Input url: ")
-	*account = accoutData{
-		login:    login,
-		password: password,
-		url:      url,
-	}
-}
-
-func printAccountData(account *accoutData) {
-	fmt.Println("Login:", account.login)
-	fmt.Println("Password:", account.password)
-	fmt.Println("URL:", account.url)
-}
-
-func generatePassword(length int) string {
-	password := make([]rune, length)
-	for i := range password {
-		password[i] = symbols[rand.IntN(len(symbols))]
-	}
-	return string(password)
+func printMenu() {
+	fmt.Println("\nВыберите действие:")
+	fmt.Println("1. Добавить данные аккаунта")
+	fmt.Println("2. Просмотреть данные аккаунта")
+	fmt.Println("3. Выход")
 }
