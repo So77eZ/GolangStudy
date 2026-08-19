b45c4714d9a9152ac63780b230c02e76ecd0c730
Slava <ja227@mail.ru>
2026-08-19 22:03:57 +0300

    feat: Добавил расшифровку зашифрованного файла как метод Encrypter.Decrypt

--- password/encrypter/encrypter.go
+++ password/encrypter/encrypter.go
@@ -3,6 +3,8 @@ package encrypter
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
+	"io"
 	"os"
 )
 
@@ -10,6 +12,7 @@ import (
 type Encrypter struct {
 	Key string
 }
+
 // NewEncrypter - создает новый экземпляр Encrypter с ключом из переменной окружения SECRET_KEY
 func NewEncrypter() *Encrypter {
 	res := os.Getenv("SECRET_KEY")
@@ -21,7 +24,7 @@ func NewEncrypter() *Encrypter {
 
 // Encrypt - метод для шифрования строки
 func (enc *Encrypter) Encrypt(plainStr []byte) []byte {
-	block,err:=aes.newCipher([]byte(enc.Key))
+	block, err := aes.NewCipher([]byte(enc.Key))
 	if err != nil {
 		panic(err.Error())
 	}
@@ -30,14 +33,28 @@ func (enc *Encrypter) Encrypt(plainStr []byte) []byte {
 		panic(err.Error())
 	}
 	nonce := make([]byte, aesGCM.NonceSize())
-	_, err:= io.ReadFull(rand.Reader, nonce)
+	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
 		panic(err.Error())
 	}
 	return aesGCM.Seal(nonce, nonce, plainStr, nil)
 }
+
 // Decrypt - метод для дешифрования строки
 func (enc *Encrypter) Decrypt(encryptedString []byte) []byte {
-	
-	return []
+	block, err := aes.NewCipher([]byte(enc.Key))
+	if err != nil {
+		panic(err.Error())
+	}
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := aesGCM.NonceSize()
+	nonce, cipherText := encryptedString[:nonceSize], encryptedString[nonceSize:]
+	plainText, err := aesGCM.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plainText
 }
